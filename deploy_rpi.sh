03dc847c95c36d5dce957ac3b0013e971b1abfae
Sylvain Renaud <sylvain.renaud@master.hes-so.ch>
2018-11-01 15:26:24 +0100

    Update deploy script.

--- deploy_rpi.sh
+++ deploy_rpi.sh
@@ -5,8 +5,6 @@ ip_addr='192.168.1.2'
 dest_folder='/home/pi/IoT-Lab-SensorTag/'
 source_folder='/home/sylvain/DATA/15_IoT/IoT-Lab-SensorTag'
 
-scp "$source_folderapp/app/backend.py" "$user@$ip_addr:$dest_folder"
-scp "$source_folderapp/app/app.py" "$user@$ip_addr:$dest_folder"
+scp "$source_folderapp/app/{backend, app}.py" "$user@$ip_addr:$dest_folder"
 
-ssh "$user@$ip_addr" "python2 -m pip install --user Flask bluepy"
-ssh "$user@$ip_addr" "python2 $dest_folder/app.py"
+ssh "$user@$ip_addr" "python2 -m pip install --user Flask bluepy; python2 $dest_folder/app.py"
